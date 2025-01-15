6485470d4794916557b36f712609e838ef1d66b6
ThomAzgo <weifang.buisson@gmail.com>
2025-01-15 11:55:55 +0100

    UserController update api key

--- Laravel/routes/api.php
+++ Laravel/routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
+Route::middleware(['auth:sanctum'])->post('/user/etherscan_api_key', 'UserController@store_etherscan_api_key');
 
 require __DIR__ . '/auth.php';
